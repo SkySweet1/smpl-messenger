01303d395e92a59983f562d510d6b54888edfb0e
timestamp <mashanovhdhh@gmail.com>
2026-04-27 23:56:52 +0300

    added description for clie.c

--- clie.c
+++ clie.c
@@ -10,11 +10,14 @@
 #define PORT 0002
 #define BUFFER_SIZE 1024
 
-void set_rgb_color(int r, int g, int b){
+void set_rgb_color(int r, int g, int b){ // установка RGB цвета
     printf("\033[38;2;%d;%d;%dm", r, g, b);
 }
+/*
+функция установки цвета
+*/
 
-void rainbow_text_smooth(const char* text){
+void rainbow_text_smooth(const char* text){ // радужный вывод
     int length = 0;
     while(text[length] != '\0') length++;
     
@@ -30,6 +33,11 @@ void rainbow_text_smooth(const char* text){
     }
     printf("\033[0m"); 
 }
+/*
+функция радужного вывода текста
+радужный текст — функция rainbow_text_smooth() использует синусоиды для плавного перехода цветов RGB
+каждый символ имеет свой цвет в зависимости от позиции в строке
+*/
 
 int main(void){
     int client_fd;
@@ -40,36 +48,42 @@ int main(void){
         rainbow_text_smooth("socket invalid\n");
         return -1;
     }
+    /*
+    создание сокета
+    */
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
+    /*
+    настройка адреса
+    */
 
-    if(inet_pton(AF_INET,"127.0.0.1", &server_addr.sin_addr) <= 0){
+    if(inet_pton(AF_INET,"127.0.0.1", &server_addr.sin_addr) <= 0){ // преобразует IP "127.0.0.1" из текста в бинарный формат
         rainbow_text_smooth("invalid address\n");
         return -1;
     }
 
-    if(connect(client_fd,(struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+    if(connect(client_fd,(struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){ // устанавливаем соединение с сервером
         rainbow_text_smooth("invalid connect\n");
         return -1;
     }
 
     rainbow_text_smooth("connect\n");
 
-    while(1){
+    while(1){ // бесконечный цикл
         fd_set readfds;
         FD_ZERO(&readfds);
         FD_SET(client_fd, &readfds);
         FD_SET(0, &readfds);
 
-        select(client_fd + 1, &readfds, NULL, NULL, NULL);
+        select(client_fd + 1, &readfds, NULL, NULL, NULL); // select ждёт ввода с клавиатуры ИЛИ сообщения от сервера
 
-        if(FD_ISSET(0, &readfds)){
+        if(FD_ISSET(0, &readfds)){ // проверяем, ввёл ли пользователь сообщение
             fgets(buffer, BUFFER_SIZE, stdin);
             send(client_fd, buffer, strlen(buffer), 0);
         }
 
-        if(FD_ISSET(client_fd, &readfds)){
+        if(FD_ISSET(client_fd, &readfds)){ // проверяем, пришло ли сообщение от сервера
             int bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0);
 
             if(bytes_received > 0){
